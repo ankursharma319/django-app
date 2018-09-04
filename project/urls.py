78aa680032051628ff83ff49e436bc7c20a570fc
Ankur Sharma <ankur_sharma309@yahoo.com>
2018-09-04 21:46:05 +0200

    updating deployment settings

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,10 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
+#url(r'^media/(?P<path>.*)$', serve, {
+#            'document_root': settings.MEDIA_ROOT,
+#            }),
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
