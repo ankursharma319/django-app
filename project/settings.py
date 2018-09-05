d8e6e7ab905878855b63ca3cb5763447ee6c9c6c
Ankur Sharma <ankur_sharma309@yahoo.com>
2018-09-05 11:41:12 +0200

    Revert "trying backup secret key"
    This reverts commit 06b4a9949b1357421cd196f0782a58ca6d5890cb.

--- project/settings.py
+++ project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # The SECRET_KEY is provided via an environment variable in OpenShift
 SECRET_KEY = os.getenv(
-    'DJANGO_SECRET_KEY',
-    '9e4@&tw46$l31)zrqe3wi+-slqm(ruvz&se0^%9#6(_w3ui!c0'
+    'DJANGO_SECRET_KEY'
     # safe value used for development when DJANGO_SECRET_KEY might not be set
 )
 
