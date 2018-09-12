1edba892abb4899d075c3a8017ef551d3739dbfd
Ankur Sharma <ankur_sharma309@yahoo.com>
2018-09-12 13:45:53 +0200

    removed social urls from urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 import welcome.views
 
@@ -13,9 +12,6 @@ urlpatterns = [
     url(r'^$', welcome.views.index, name='home'),
     url(r'^health$', welcome.views.health),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
 
 #url(r'^media/(?P<path>.*)$', serve, {	
